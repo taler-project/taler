20a739971e8a232dc9e5e75e050e245690fed7e0
Mikalai Ramanovich <MikalaiR@users.noreply.github.com>
2018-08-23 20:57:23 +0300

    Change official site (part 1/3)

--- src/init.cpp
+++ src/init.cpp
@@ -526,7 +526,7 @@ std::string HelpMessage(HelpMessageMode mode)
 std::string LicenseInfo()
 {
     const std::string URL_SOURCE_CODE = "<https://github.com/taler-project/taler>";
-    const std::string URL_WEBSITE = "<https://taler.site>";
+    const std::string URL_WEBSITE = "<https://talercrypto.com>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2016, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
